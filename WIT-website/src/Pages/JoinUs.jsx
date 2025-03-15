960df8de90a6b6ce86dad42f8b15323b5c5a858d
rane20 <145800950+rane20@users.noreply.github.com>
2025-03-14 19:05:27 -0700

    embedded join us google form

--- WIT-website/src/Pages/JoinUs.jsx
+++ WIT-website/src/Pages/JoinUs.jsx
@@ -1,12 +1,43 @@
-import { useEffect } from 'react';
+import React from "react";
+import "../Styles/JoinUs.css";
+import instagramLogo from '../assets/Logo/instaPurple.png';
+import discordLogo from '../assets/Logo/discordPurple.png';
 
 function JoinUs() {
-  useEffect(() => {
-    // Redirect to the Google Form link
-    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfFZKpcMzfrsDnQopEoeKCzhD2rrWzPALADaOaxjcTWUdUcRg/viewform";
-  }, []);
+  return (
+    <div className="join-us-container">
+            <h1 className="community-title">Join Our Community!</h1>
 
-  return null; // No need to render anything
+      {/* Social Media Links with Images */}
+      <div className="image-links">
+        <a
+          href="https://www.instagram.com/calpolywit/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-link"
+        >
+          <img src={instagramLogo} alt="Instagram" className="social-logo" />
+        </a>
+        <a
+          href="https://discord.com/invite/R5nrdnDFTq"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-link"
+        >
+          <img src={discordLogo} alt="Discord" className="social-logo" />
+        </a>
+       
+      </div>
+
+      {/* Embedded Google Form */}
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfFZKpcMzfrsDnQopEoeKCzhD2rrWzPALADaOaxjcTWUdUcRg/viewform?embedded=true"
+        className="google-form"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
 }
 
 export default JoinUs;
